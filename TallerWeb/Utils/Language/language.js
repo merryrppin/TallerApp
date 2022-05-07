0f3b8a712ff132a5696cd70b40308c255ffab49d
Wilmar González <merryrppin@hotmail.com>
2022-05-06 22:16:42 -0500

    configuración para el correcto funcionamiento del sitio web

---
+++ TallerWeb/Utils/Language/language.js
@@ -0,0 +1,201 @@
+﻿var aLanguage = {
+    'appName': 'erpApp',
+    'sign_in_app': 'Ingresar a ERP',
+
+    'english': 'Inglés',
+    'spanish': 'Español',
+    'german': 'Alemán',
+
+
+    'home': 'Inicio',
+    'actions': 'Acciones',
+    'email': 'Email',
+    'password': 'Contraseña',
+    'remember_me': 'Recordarme',
+    'forget_password': '¿Olvidó su contraseña?',
+    'sign_in': 'Ingresar',
+    'dont_have_account_yet': '¿No tiene cuenta aún?',
+    'sign_up': 'Registrarse',
+    'sign_out': 'Cerrar sesión',
+    'name': 'Nombre',
+    'surnames': 'Apellidos',
+    'add': 'Agregar',
+    'addUser': 'Agregar usuario',
+    'editUser': 'Editar usuario',
+    'enterName': 'Ingrese el nombre',
+    'PleaseEnterName': 'Por favor ingrese el nombre',
+    'enterSurnames': 'Ingrese los apellidos',
+    'PleaseEnterSurnames': 'Por favor ingrese los apellidos',
+    'enterEmail': 'Ingrese Email',
+    'PleaseEnterEmail': 'Por favor ingrese Email',
+    'username': 'Usuario',
+    'enterUsername': 'Ingrese usuario',
+    'PleaseEnterUsername': 'Por favor ingrese usuario',
+    'enterPassword': 'Ingrese contraseña',
+    'PleaseEnterPassword': 'Por favor ingrese contraseña',
+    'repeatPassword': 'Repita contraseña',
+    'enterRepeatPassword': 'Ingrese contraseña nuevamente',
+    'PleaseEnterRepeatPassword': 'Por favor ingrese contraseña nuevamente',
+    'errorUserExist': 'El usuario ya existe',
+    'save': 'Guardar',
+    'cancel': 'Cancelar',
+    'accept': 'Aceptar',
+    'clean': 'Limpiar',
+    'saveSuccessful': 'Registro guardado exitosamente',
+    'fatalError': 'Ha ocurrido un error',
+    'generalError': 'Ha ocurrido un error',
+    'hi': 'Hola',
+
+    //Warehouse
+    'warehouseCode': 'Código Bodega',
+    'description': 'Descripción',
+    'default': 'Por Defecto',
+    'inactive': 'Inactivo',
+    'addWarehouse': 'Agregar Bodega',
+    'editWarehouse': 'Editar Bodega',
+
+    //Series
+    'SerieCode': 'Código serie',
+    'InitialNumber': 'Numero Inicial',
+    'CurrentNumber': 'Numero Actual',
+    'FinalNumber': 'Numero Final',
+    'Prefix': 'Prefijo',
+    'Suffix': 'Sufijo',
+    'Title': 'Titulo',
+    'SubTitle': 'Subtitulo',
+    'Legend1': 'Leyenda 1',
+    'Legend2': 'Leyenda 2',
+    'Legend3': 'Leyenda 3',
+    'Legend4': 'Leyenda 4',
+    'Legend5': 'Leyenda 5',
+    'Legend6': 'Leyenda 6',
+    'Legend7': 'Leyenda 7',
+    'Legend8': 'Leyenda 8',
+    'Legend9': 'Leyenda 9',
+    'Legend10': 'Leyenda 10',
+    'Default': 'Por Defecto',
+    'Active': 'Activo',
+    'addSerie': 'Agregar Serie',
+    'editSerie': 'Editar Serie',
+
+    //PriceList
+    'codArtFrom': 'Cod Art Desde',
+    'codArtTo': 'Cod Art Hasta',
+    'priceList': 'Lista Precio',
+    'line': 'Linea',
+    'subLine': 'SubLinea',
+    'basePrice': 'Precio Base',
+    'initialDate': 'Fecha Inicial',
+    'endDate': 'Fecha Final',
+    'factor': 'Factor',
+    'generate': 'Generar',
+    'clearFilter': 'Limpiar',
+    'search': 'Buscar', 
+
+    //SaleInvoice
+    'code': 'Código',
+    'productName': 'Nombre del producto',
+    'amount': 'Cantidad',
+    'price': 'Precio',
+    'totalWithoutIVA': 'Total sin IVA',
+    'IVA': 'IVA',
+    'discount': 'Descuento',
+    'totalDiscount': 'Descuento total',
+    'price': 'Precio',
+    'netTotal': 'Total Neto',
+    'document': 'Documento',
+    'clientName': 'Nombre del cliente',
+    'clientInfo': 'Información del cliente',
+    'articleCode': 'Código articulo',
+    'notProductFound': 'No se encontró el producto',
+    'selectOption': 'Seleccione una opción',
+    'selectVendor': 'Seleccione un vendedor',
+    'selectWarehouse': 'Seleccione una bodega',    
+    'selectPriceList': 'Seleccione una lista de precios',    
+    'vendorInfo': 'Información del vendedor',
+    'addProduct': 'Añadir producto',
+    'yes': 'Si',
+    'no': 'No',
+    'AreYouSure': '¿Está seguro?',
+    'AreYouSureContinue': '¿Está seguro que desea continuar?',
+    'NotSavedSaleInvoice': 'La factura de venta no se ha guardado, si genera una nueva perderá todos los cambios',
+    'addAtLeastOneItem':'Debe agregar por lo menos un producto a la factura',
+    
+    'localeTextAgGrid': {
+        // for filter panel
+        page: 'Página',
+        more: 'Máx',
+        to: 'Hasta',
+        of: 'De',
+        next: 'Siguiente',
+        last: 'Última',
+        first: 'Primera',
+        previous: 'Anterior',
+        loadingOoo: 'Cargando...',
+
+        // for set filter
+        selectAll: 'Seleccionar todas',
+        searchOoo: 'Buscar...',
+        blanks: 'Blancas',
+
+        // for number filter and text filter
+        filterOoo: 'Flitradas...',
+        equals: 'Iguales',
+        notEqual: 'Diferentes',
+
+        // for the date filter
+        dateFormatOoo: 'YYYY-mm-dd',
+
+        // for number filter
+        lessThan: 'Menores que',
+        greaterThan: 'Mayores que',
+        lessThanOrEqual: 'Menores o iguales',
+        greaterThanOrEqual: 'Mayores o iguales',
+        inRange: 'En el rango',
+        inRangeStart: 'Rango inicio',
+        inRangeEnd: 'Rango fin',
+
+        // for text filter
+        contains: 'Contiene',
+        notContains: 'No contiene',
+        startsWith: 'Inicia con',
+        endsWith: 'Finaliza con',
+
+        // filter conditions
+        andCondition: 'Y',
+        orCondition: 'O',
+
+        // filter buttons
+        applyFilter: 'Aplicar',
+        resetFilter: 'Resetear',
+        clearFilter: 'Limpiar',
+
+        // the header of the default group column
+        group: 'Agrupar',
+
+        // tool panel
+        columns: 'Columnas',
+        filters: 'Filtros',
+        rowGroupColumns: 'Pivotear columnas',
+        rowGroupColumnsEmptyMessage: 'la drag cols to group',
+        valueColumns: 'Valor columnas',
+        pivotMode: 'Pivot-Mode',
+        groups: 'Grupos',
+        values: 'Valores',
+        pivots: 'Pivotes',
+        valueColumnsEmptyMessage: 'la drag cols to aggregate',
+        pivotColumnsEmptyMessage: 'la drag here to pivot',
+        toolPanelButton: 'Panel de herramientas',
+
+        // other
+        noRowsToShow: 'Sin filas',
+        enabled: 'Habilitado',
+        
+        // standard menu
+        copy: 'Copiar',
+        copyWithHeaders: 'Copiar con encabezados',
+        ctrlC: 'Ctrl + C',
+        paste: 'Pegar',
+        ctrlV: 'Ctrl + V'
+    }
+};
---
+++ TallerWeb/app/modules/general/generalCtrl.js
@@ -0,0 +1,175 @@
+﻿angular.module(aLanguage.appName).controller('generalController', ["$scope", '$rootScope', "$timeout", "$filter", "$location", "SessionService", "GeneralService", generalController]);
+function generalController($scope, $rootScope, $timeout, $filter, $location, SessionService, GeneralService) {
+    var ctrl = this;
+    ctrl.selectedLanguage = GeneralService.selectedLanguage;
+    ctrl.aLanguage = aLanguage;
+    ctrl.allLevels = [];
+    ctrl.menusByLevel = [];
+    ctrl.menus = [];
+    ctrl.allMenus = [];
+    ctrl.currentMenuIdByLevel = [];
+    ctrl.currentUrl = "-";
+    
+    ctrl.showNewButton = $rootScope.showNewButton;
+    ctrl.showSaveButton = $rootScope.showSaveButton;
+    ctrl.showClearButton = $rootScope.showClearButton;
+    ctrl.showCancelButton = $rootScope.showCancelButton;
+    ctrl.showPrintButton = $rootScope.showPrintButton;
+    
+    $rootScope.$broadcast('restorestate');
+    GeneralService.userLogin = SessionService.model;
+    ctrl.userLogin = GeneralService.userLogin;
+
+    ctrl.autentication = GeneralService.autentication;//false por defecto
+
+    $scope.$watch(GeneralService.autentication, function (change) { ///adding watcher on someService.getChange, it will fire when change changes value
+        ctrl.autentication = GeneralService.autentication; //setting change to controller here you can put some extra logic
+    }.bind(this));
+
+    ctrl.loadDataFromGeneralService = function () {
+        //$scope.contentTitle = GeneralService.contentTitle;
+    };
+
+    ctrl.verificarAutenticacion = function () {
+        //TODO seteo de variable a true hasta terminar de tener los usuarios definidos
+        GeneralService.autentication.isAuthenticated = true;
+        GeneralService.showPanels();
+        //Fin seteo variables
+
+        if (typeof GeneralService.userLogin !== 'undefined' && typeof GeneralService.userLogin.UserId !== 'undefined' && GeneralService.userLogin.UserId !== null) {
+            GeneralService.autentication.isAuthenticated = true;
+            GeneralService.showPanels();
+        } else {
+            window.location.hash = "#!/login";
+            window.location.pathname = "Login.html";
+        }
+    };
+
+    ctrl.loadMenus = function () {
+        var dataSP = {
+            "StoredProcedureName": "GetActiveMenus",
+            "StoredParams": []
+        };
+        GeneralService.executeAjax({
+            url: 'api/executeStoredProcedure',
+            data: dataSP,
+            success: function (response) {
+                if (response.Exception === null) {
+                    ctrl.orderMenus(response.Value[0].DataMapped);
+                }
+            }
+        });
+    };
+
+    ctrl.loadAllLevels = function () {
+        ctrl.allLevels = ctrl.allMenus.map(function (menuData) {
+            return {
+                Level: menuData.Level
+            };
+        }).unique("Level");
+    };
+
+    ctrl.loadMenusByLevels = function () {
+        ctrl.menusByLevel = [];
+        $.each(ctrl.allLevels, function (i, levelMenu) {
+            var objMenu = { 'Level': levelMenu.Level, 'Menus': $filter('filter')(ctrl.allMenus, { 'Level': levelMenu.Level }) };
+            ctrl.menusByLevel.push(objMenu);
+        });
+    };
+
+    ctrl.orderMenus = function (data) {
+        ctrl.allMenus = angular.copy(data);
+        ctrl.loadAllLevels();
+        ctrl.loadMenusByLevels();
+        $.each(ctrl.menusByLevel, function (i, menuByLevel) {
+            var nextLevel = i + 1;
+            $.each(menuByLevel.Menus, function (j, objMenu) {
+                if (nextLevel < ctrl.menusByLevel.length) {
+                    var parentMenu = $filter('filter')(ctrl.menusByLevel[nextLevel].Menus, { 'MenuId': objMenu.ParentMenuId }, true);
+                    if (parentMenu.length > 0) {
+                        if (typeof parentMenu[0].MenusByLevel === 'undefined') parentMenu[0].MenusByLevel = [];
+                        parentMenu[0].MenusByLevel.push(objMenu);
+                    }
+                } else {
+                    ctrl.menus.push(objMenu);
+                }
+            });
+        });
+        GeneralService.userLogin.Menus = angular.copy(ctrl.menus);
+        GeneralService.userLogin.menusByLevel = angular.copy(ctrl.menusByLevel);
+        GeneralService.userLogin.allMenus = angular.copy(ctrl.allMenus);
+    };
+
+    ctrl.recursiveMenus = function (objMenu) {
+        objMenu.CurrentLevel++;
+        if (objMenu.CurrentLevel >= objMenu.Level) {
+            $.each(ctrl.menus);
+        } else {
+            ctrl.recursiveMenus(objMenu);
+        }
+    };
+
+    ctrl.signOut = function () {
+        $rootScope.$broadcast('clearState');
+        window.location.hash = "#!/login";
+        window.location.pathname = "Login.html";
+    };
+
+    //BEGIN General Buttons
+    ctrl.saveBtnFunction = $rootScope.saveBtnFunction;
+    ctrl.saveBtnFunction = $rootScope.clearBtnFunction;
+    ctrl.saveBtnFunction = $rootScope.cancelBtnFunction;
+    //END General Buttons
+
+    ctrl.changeMenu = function () {
+        $timeout(function () {
+            ctrl.extractFromUrl();
+        }, 500);
+    };
+
+    ctrl.extractFromUrl = function () {
+        ctrl.currentMenuIdByLevel = [];
+        ctrl.loadAllLevels();
+        $.each(ctrl.allLevels, function (i, obj) {
+            ctrl.currentMenuIdByLevel[parseInt(obj.Level)] = -1;
+        });
+        ctrl.currentUrl = window.location.hash;
+        var aCurrentMenu = $filter('filter')(ctrl.allMenus, { 'Url': ctrl.currentUrl }, true);
+        if (aCurrentMenu.length > 0) {
+            var objCurrentMenu = angular.copy(aCurrentMenu[0]);
+            ctrl.currentMenuIdByLevel[parseInt(objCurrentMenu.Level)] = objCurrentMenu.MenuId;
+            if (objCurrentMenu.ParentMenuId !== "") {
+                var aCurrentParentMenu = $filter('filter')(ctrl.allMenus, { 'MenuId': objCurrentMenu.ParentMenuId }, true);
+                if (aCurrentParentMenu.length > 0) {
+                    var objCurrentParentMenu = angular.copy(aCurrentParentMenu[0]);
+                    ctrl.currentMenuIdByLevel[parseInt(objCurrentParentMenu.Level)] = objCurrentParentMenu.MenuId;
+                    if (objCurrentParentMenu.ParentMenuId !== "") {
+                        var aCurrentLastParentMenu = $filter('filter')(ctrl.allMenus, { 'MenuId': objCurrentParentMenu.ParentMenuId }, true);
+                        if (aCurrentLastParentMenu.length > 0) {
+                            var objCurrentLastParentMenu = angular.copy(aCurrentLastParentMenu[0]);
+                            ctrl.currentMenuIdByLevel[parseInt(objCurrentLastParentMenu.Level)] = objCurrentLastParentMenu.MenuId;
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    angular.element(document).ready(init);
+
+    function init() {
+        ctrl.verificarAutenticacion();
+        if (ctrl.autentication.isAuthenticated) {
+            ctrl.loadDataFromGeneralService();
+            if (typeof GeneralService.userLogin.Menus === 'undefined')
+                ctrl.loadMenus();
+            else if (ctrl.menus.length === 0) {
+                ctrl.menus = angular.copy(GeneralService.userLogin.Menus);
+                ctrl.menusByLevel = angular.copy(GeneralService.userLogin.menusByLevel);
+                ctrl.allMenus = angular.copy(GeneralService.userLogin.allMenus);
+                ctrl.extractFromUrl();
+            }
+            ctrl.changeMenu();
+        }
+    }
+}

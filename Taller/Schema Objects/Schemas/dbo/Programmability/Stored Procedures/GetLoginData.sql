80701e0051d3a10ead154a54df6901e55c287a7e
Wilmar González <merryrppin@hotmail.com>
2022-05-06 21:44:45 -0500

    Actualización del esquema param

--- Taller/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetLoginData.sql
+++ Taller/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetLoginData.sql
@@ -5,7 +5,4 @@ AS BEGIN
 	SELECT u.UserId, u.UserFirstName, u.UserLastName, CONCAT(u.UserFirstName, ' ', u.UserLastName) AS UserCompleteName
 	FROM tblUsers AS u
 	WHERE [Login] = @login AND [Password] = @password
-
-	SELECT TOP 1 CompanyId, CompanyName, Country, NIT
-	FROM tblCompanies
 END

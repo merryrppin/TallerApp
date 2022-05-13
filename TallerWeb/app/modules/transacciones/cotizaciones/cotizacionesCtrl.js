d4f0f6460c9958a820d2cb33faa63c1d4bd02189
Wilmar González <merryrppin@hotmail.com>
2022-05-13 18:45:04 -0500

    cargue de datos de la tabla de productos

--- TallerWeb/app/modules/transacciones/cotizaciones/cotizacionesCtrl.js
+++ TallerWeb/app/modules/transacciones/cotizaciones/cotizacionesCtrl.js
@@ -20,24 +20,28 @@ function cotizacionesController($scope, $rootScope, $location, GeneralService) {
         cliente: '',
         fechaElaboracion: moment().format('YYYY/MM/DD'),
         contacto: '',
-        responsableCotizacion: '',
+        responsableCotizacion: GeneralService.userLogin.UserCompleteName,
+        responsableCotizacionId: GeneralService.userLogin.UserId,
         encabezado: '',
         condicionesComerciales: '',
     };
 
     $scope.newProduct = {
-        productId: '',
+        descripcion: '',
+        productId: null,
         cantidad: 0,
-        valorunitario: 0.0,
-        descuento: 0.0,
-        impuestocargo: 0.0,
-        impuestoretencion: 0.0,
-        totalProducto: 0.0
+        available_quantity: 0,
+        valorunitario: 0,
+        descuento: 0,
+        taxes: 0,
+        impuestoretencion: 0,
+        totalProducto: 0,
+        taxesList: [],
+        taxId: '-1'
     };
 
     $scope.productList = [];
     $scope.dataGridProduct = [];
-    //$scope.dataGridProduct = [angular.copy($scope.newProduct)];
     $scope.customerList = [];
     $scope.contactos = [];
 
@@ -54,6 +58,11 @@ function cotizacionesController($scope, $rootScope, $location, GeneralService) {
                 if (response.Exception === null) {
                     $scope.customerList = response.Value[0].DataMapped.map(function (objCustomer) {
                         objCustomer.name = objCustomer.name.replace('["', '').replace('"]', '');
+                        var aContacts = [];
+                        if (objCustomer.contacts != null && objCustomer.contacts != '') {
+                            aContacts = JSON.parse(objCustomer.contacts);
+                        }
+                        objCustomer.contactos = aContacts;
                         return objCustomer;
                     });
                 }
@@ -91,37 +100,61 @@ function cotizacionesController($scope, $rootScope, $location, GeneralService) {
         $('[data-toggle="tooltip"]').tooltip('dispose');
         $scope.dataGridProduct.splice(rowIndex, 1);
         $('[data-toggle="tooltip"]').tooltip();
-        recalculateTotal()
+        $scope.recalculateTotal();
     };
+
+    $scope.selectCliente = function () {
+        $scope.contactos = angular.copy($scope.customerList.filter(p => p.id === $scope.cotizacion.cliente)[0]);
+    };
+
+    var taxZeroOption = { id: -1, name: "Ninguno", percentage: 0 };
+
+    $scope.setProduct = function (product) {
+        var selectedProduct = $scope.productList.filter(p => p.id === product.productoId)[0];
+        product.available_quantity = parseFloat(selectedProduct.available_quantity);
+        var taxesL = JSON.parse(selectedProduct.taxes);
+        if (taxesL.length === 0)
+            taxesL.push(taxZeroOption);
+        product.taxesList = taxesL;
+        product.taxes = taxesL[0].percentage;
+        product.taxId = taxesL[0].id.toString();
+        if (selectedProduct.prices !== "") {
+            product.valorunitario = JSON.parse(selectedProduct.prices)[0].price_list[0].value;
+        }
+    };
+
+    $scope.setTax = function (product) {
+        product.taxes = product.taxesList.filter(t => t.id.toString() === product.taxId.toString())[0].percentage;
+        $scope.recalculateTotal(product);
+    }
 
     $scope.recalculateTotal = function (product) {
         if (typeof product !== 'undefined') {
             var valorPorCantidad = product.cantidad * product.valorunitario;
             var descuentoTotal = product.cantidad * ((product.descuento / 100) * product.valorunitario);
-            var impuestoCargo = product.cantidad * ((product.impuestocargo / 100) * product.valorunitario);
-            var impuestoRetencion = product.cantidad * ((product.impuestoretencion / 100) * product.valorunitario);
-            var valorTotal = valorPorCantidad - descuentoTotal + impuestoCargo + impuestoRetencion;
+            var impuestoCargo = product.cantidad * ((product.taxes / 100) * product.valorunitario);
+            var valorTotal = valorPorCantidad - descuentoTotal + impuestoCargo;
             product.totalProducto = valorTotal;
             product.descuentoTotal = descuentoTotal;
+            product.impuestoRetencion = descuentoTotal;
+            product.impuestoCargo = impuestoCargo;
         }
 
         var totalBruto = 0;
         var totalDescuentos = 0;
         var subTotal = 0;
-        var subTotalNeto = 0;
+        var impuestoTotal = 0;
         $.each($scope.dataGridProduct, function (i, objProduct) {
-            totalBruto += objProduct.totalProducto;
+            totalBruto += objProduct.cantidad * objProduct.valorunitario;
             totalDescuentos += objProduct.descuentoTotal;
+            subTotal += totalBruto - totalDescuentos;
+            impuestoTotal += objProduct.impuestoCargo;
         });
 
         $scope.total.totalBruto = totalBruto;
         $scope.total.descuentos = totalDescuentos;
-        //$scope.total = {
-        //    totalBruto: '0.00',
-        //    descuentos: '0.00',
-        //    subTotal: '0.00',
-        //    totalNeto: '0.00',
-        //};
+        $scope.total.subTotal = subTotal;
+        $scope.total.totalNeto = $scope.total.subTotal + impuestoTotal;
     };
 
     angular.element(document).ready(init);

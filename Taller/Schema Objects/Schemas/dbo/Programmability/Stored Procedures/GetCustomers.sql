18026f13e83665de17f04511c8bd22eb9d153260
Wilmar González <merryrppin@hotmail.com>
2022-05-13 15:59:35 -0500

    calculos en cotizacion

---
+++ Taller/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetCustomers.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[GetCustomers] AS
+BEGIN
+	SELECT [id], [type], [person_type], [id_type], [identification], [branch_office], [check_digit], [name], [comercial_name], [active], [vat_responsible], [fiscal_responsibilities], [address], [phones], [contacts], [comments]
+	FROM [dbo].[tblCustomers] 
+	WHERE person_type = 'Company'
+END
--- TallerWeb/app/modules/transacciones/cotizaciones/cotizacionesCtrl.js
+++ TallerWeb/app/modules/transacciones/cotizaciones/cotizacionesCtrl.js
@@ -1,4 +1,6 @@
-﻿angular.module(aLanguage.appName).controller('cotizacionesController', ["$scope", '$rootScope', "$location", "GeneralService", cotizacionesController]);
+﻿agGrid.initialiseAgGridWithAngular1(angular);
+var module = angular.module(aLanguage.appName, ["agGrid"]);
+module.controller('cotizacionesController', ["$scope", '$rootScope', "$location", "GeneralService", cotizacionesController]);
 function cotizacionesController($scope, $rootScope, $location, GeneralService) {
 
     GeneralService.hideGeneralButtons();
@@ -13,7 +15,7 @@ function cotizacionesController($scope, $rootScope, $location, GeneralService) {
     };
 
     $scope.cotizacion = {
-        tipo: '',
+        tipo: { 'Name': 'C-1-Cotización' },
         numero: '',
         cliente: '',
         fechaElaboracion: moment().format('YYYY/MM/DD'),
@@ -23,70 +25,119 @@ function cotizacionesController($scope, $rootScope, $location, GeneralService) {
         condicionesComerciales: '',
     };
 
-    $scope.loadUser = function () {
-        //var dataSP = {
-        //    "StoredProcedureName": "GetUser",
-        //    "StoredParams": [{
-        //        Name: "UserId",
-        //        Value: $scope.userIdSelected
-        //    }]
-        //};
-
-        //GeneralService.executeAjax({
-        //    url: 'api/executeStoredProcedure',
-        //    data: dataSP,
-        //    success: function (response) {
-        //        if (response.Value.length === 1) {
-        //            $scope.currentUser = response.Value[0].DataMapped[0];
-        //        } else {
-        //            GeneralService.showToastR({
-        //                body: aLanguage[response.GeneralError],
-        //                type: 'error'
-        //            });
-        //        }
-        //    }
-        //});
+    $scope.newProduct = {
+        productId: '',
+        cantidad: 0,
+        valorunitario: 0.0,
+        descuento: 0.0,
+        impuestocargo: 0.0,
+        impuestoretencion: 0.0,
+        totalProducto: 0.0
     };
 
-    $scope.columnDefs = [
-        { headerName: "Producto", field: "UserFirstName" },
-        { headerName: "Descripción", field: "UserFirstName" },
-        { headerName: "Cant", field: "UserFirstName" },
-        { headerName: "Valor Unitario", field: "UserFirstName" },
-        { headerName: "% Desc.", field: "UserFirstName" },
-        { headerName: "Impuesto Cargo", field: "UserFirstName" },
-        { headerName: "Impuesto Retención", field: "UserFirstName" },
-        { headerName: "Valor Total", field: "UserFirstName" }
-    ];
-
     $scope.productList = [];
+    $scope.dataGridProduct = [];
+    //$scope.dataGridProduct = [angular.copy($scope.newProduct)];
+    $scope.customerList = [];
+    $scope.contactos = [];
 
-    $scope.listProductsGrid = {
-        localeText: $scope.aLanguage.localeTextAgGrid,
-        columnDefs: $scope.columnDefs,
-        rowData: $scope.productList,
-        suppressRowClickSelection: true,
-        enableColResize: true,
-        defaultColDef: {
-            sortable: false,
-            filter: false,
-            resize: true
-        },
-        angularCompileRows: true
+    $scope.loadCustomers = function () {
+        var dataSP = {
+            "StoredProcedureName": "GetCustomers",
+            "StoredParams": []
+        };
+
+        GeneralService.executeAjax({
+            url: 'api/executeStoredProcedure',
+            data: dataSP,
+            success: function (response) {
+                if (response.Exception === null) {
+                    $scope.customerList = response.Value[0].DataMapped.map(function (objCustomer) {
+                        objCustomer.name = objCustomer.name.replace('["', '').replace('"]', '');
+                        return objCustomer;
+                    });
+                }
+            }
+        });
+    };
+
+    $scope.loadProducts = function () {
+        var dataSP = {
+            "StoredProcedureName": "GetProducts",
+            "StoredParams": []
+        };
+
+        GeneralService.executeAjax({
+            url: 'api/executeStoredProcedure',
+            data: dataSP,
+            success: function (response) {
+                if (response.Exception === null) {
+                    $scope.productList = angular.copy(response.Value[0].DataMapped);
+                }
+            }
+        });
+    };
+
+    $scope.addProduct = function () {
+        rowPosition = $scope.dataGridProduct.length;
+        $scope.dataGridProduct.push(angular.copy($scope.newProduct));
+        $('[data-toggle="tooltip"]').tooltip();
+        $('#slProducto' + rowPosition).select2({
+            placeholder: "Seleccione una opción"
+        });
+    };
+
+    $scope.removeProducto = function (rowIndex) {
+        $('[data-toggle="tooltip"]').tooltip('dispose');
+        $scope.dataGridProduct.splice(rowIndex, 1);
+        $('[data-toggle="tooltip"]').tooltip();
+        recalculateTotal()
+    };
+
+    $scope.recalculateTotal = function (product) {
+        if (typeof product !== 'undefined') {
+            var valorPorCantidad = product.cantidad * product.valorunitario;
+            var descuentoTotal = product.cantidad * ((product.descuento / 100) * product.valorunitario);
+            var impuestoCargo = product.cantidad * ((product.impuestocargo / 100) * product.valorunitario);
+            var impuestoRetencion = product.cantidad * ((product.impuestoretencion / 100) * product.valorunitario);
+            var valorTotal = valorPorCantidad - descuentoTotal + impuestoCargo + impuestoRetencion;
+            product.totalProducto = valorTotal;
+            product.descuentoTotal = descuentoTotal;
+        }
+
+        var totalBruto = 0;
+        var totalDescuentos = 0;
+        var subTotal = 0;
+        var subTotalNeto = 0;
+        $.each($scope.dataGridProduct, function (i, objProduct) {
+            totalBruto += objProduct.totalProducto;
+            totalDescuentos += objProduct.descuentoTotal;
+        });
+
+        $scope.total.totalBruto = totalBruto;
+        $scope.total.descuentos = totalDescuentos;
+        //$scope.total = {
+        //    totalBruto: '0.00',
+        //    descuentos: '0.00',
+        //    subTotal: '0.00',
+        //    totalNeto: '0.00',
+        //};
     };
 
     angular.element(document).ready(init);
 
     function init() {
-        if ($scope.userIdSelected !== -1)
-            $scope.loadUser();
+        $scope.loadCustomers();
+        $scope.loadProducts();
     }
 
     $(document).ready(function () {
         $('.datepicker').datepicker({
             format: 'yyyy/mm/dd'
         });
-        $('.select2cls').select2();
+        $('.select2cls').select2({
+            placeholder: "Seleccione una opción"
+        });
         $("#frmUser").validate({
             rules: {
                 userFirstName: {
